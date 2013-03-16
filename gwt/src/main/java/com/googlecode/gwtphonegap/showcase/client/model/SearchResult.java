0609d590fbf9b350ebb6df637843d071139c54be
Nathan Frost <nathan.frost@gmail.com>
2013-03-16 15:33:51 -0700

    Fill Structure for Previous Commit

--- gwt/src/main/java/com/googlecode/gwtphonegap/showcase/client/model/SearchResult.java
+++ gwt/src/main/java/com/googlecode/gwtphonegap/showcase/client/model/SearchResult.java
@@ -7,5 +7,21 @@ package com.googlecode.gwtphonegap.showcase.client.model;
  * Time: 3:06 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SearchResult {
+public class SearchResult extends PGModule{
+    String time;
+    String location;
+    public SearchResult(String name, String time, String location)
+    {
+        super(name);
+        this.time = time;
+        this.location = location;
+    }
+    public String getTime()
+    {
+        return time;
+    }
+    public String getLocation()
+    {
+        return location;
+    }
 }
--- gwt/src/main/java/com/googlecode/gwtphonegap/showcase/client/widgets/BasicCellSearchDetailImpl.java
+++ gwt/src/main/java/com/googlecode/gwtphonegap/showcase/client/widgets/BasicCellSearchDetailImpl.java
@@ -1,5 +1,7 @@
 package com.googlecode.gwtphonegap.showcase.client.widgets;
 
+import com.googlecode.gwtphonegap.showcase.client.model.SearchResult;
+
 /**
  * Created with IntelliJ IDEA.
  * User: nathanfrost
@@ -7,5 +9,16 @@ package com.googlecode.gwtphonegap.showcase.client.widgets;
  * Time: 2:59 PM
  * To change this template use File | Settings | File Templates.
  */
-public class BasicCellSearchDetailImpl {
+public class BasicCellSearchDetailImpl extends BasicCell<SearchResult> {
+
+    @Override
+    public String getDisplayString(SearchResult model) {
+        String html = "<h1>" + model.getName() + "</h1><h2>" + model.getTime() + "</h2><h2>" + model.getLocation() + "</h2>";
+        return html;
+    }
+
+    @Override
+    public boolean canBeSelected(SearchResult model) {
+        return true;
+    }
 }
