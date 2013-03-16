53dec40ee5ee3c06f5ee15698041eea122a9e910
Nathan Frost <nathan.frost@gmail.com>
2013-03-16 15:28:03 -0700

    Add SearchResult Cell Impelmentations for listing ride results

---
+++ gwt/src/main/java/com/googlecode/gwtphonegap/showcase/client/model/SearchResult.java
@@ -0,0 +1,11 @@
+package com.googlecode.gwtphonegap.showcase.client.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nathanfrost
+ * Date: 3/16/13
+ * Time: 3:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchResult {
+}
---
+++ gwt/src/main/java/com/googlecode/gwtphonegap/showcase/client/widgets/BasicCellSearchDetailImpl.java
@@ -0,0 +1,11 @@
+package com.googlecode.gwtphonegap.showcase.client.widgets;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nathanfrost
+ * Date: 3/16/13
+ * Time: 2:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BasicCellSearchDetailImpl {
+}
