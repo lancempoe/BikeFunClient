7130a0a449cee259fc5a3e4030d16c31ea02934c
LanceP <lancepoehler@gmail.com>
2013-04-07 13:16:09 -0700

    Stubbing out the requests.

---
+++ gwt/src/main/java/com/bikefunfinder/client/shared/request/NewTrackRequest.java
@@ -0,0 +1,11 @@
+package com.bikefunfinder.client.shared.request;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lancepoehler
+ * Date: 4/7/13
+ * Time: 1:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewTrackRequest {
+}
---
+++ gwt/src/main/java/com/bikefunfinder/client/shared/request/UpdateEventRequest.java
@@ -0,0 +1,11 @@
+package com.bikefunfinder.client.shared.request;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lancepoehler
+ * Date: 4/7/13
+ * Time: 1:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UpdateEventRequest {
+}
