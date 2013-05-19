3fdd0a7df9b89336b650e9ee5825a0ef62b5a70a
Neuwerth, Tim <tneuwerth@ebay.com>
2013-05-19 16:18:00 -0700

    temp ip until dns

--- gwt/src/main/java/com/bikefunfinder/client/shared/constants/Settings.java
+++ gwt/src/main/java/com/bikefunfinder/client/shared/constants/Settings.java
@@ -16,6 +16,6 @@ public class Settings {
     //public static String HOST = "???";
 
     //PRD:
-    public static String HOST = "http://service.bikefunfinder.com/";
+    public static String HOST = "http://24.21.204.4/";
 
 }
