c90937f41163efd7b1c11c5dea27cad310b9ea5f
Nathan Frost <nathan.frost@gmail.com>
2013-05-20 20:21:13 -0700

    Changed MobiScroll displays (less buggy) \-._.--./  `-._.-'       I       I      bug       I     -^- sorry for the bad ascii art i hope it turns out ;(

--- android/assets/www/scripts/timepicker.js
+++ android/assets/www/scripts/timepicker.js
@@ -3,7 +3,7 @@ function setupTimePicker() {
 //http://demo.mobiscroll.com/datetime/time#mode=mixed&display=modal&theme=android-ics+light
     $('#timepicker').mobiscroll().time({
         theme: 'android-ics light',
-        display: 'modal',
+        display: 'bottom',
         mode: 'mixed',
         onSelect: function(valueText,inst){
             $('#timepicker').text(valueText);
