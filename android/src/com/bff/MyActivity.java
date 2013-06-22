c81eae7dde34bb1153e7bb60ff4cb9c059762e52
Nathan Frost <nathan.frost@gmail.com>
2013-06-22 15:47:45 -0700

    Better loading icon. Disable external links on android

--- android/src/com/bff/MyActivity.java
+++ android/src/com/bff/MyActivity.java
@@ -4,6 +4,8 @@ package com.bff;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.webkit.WebView;
 import org.apache.cordova.*;
 
 public class MyActivity extends DroidGap {
@@ -13,6 +15,31 @@ public class MyActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.init();
+
+        this.appView.clearCache(true);
+        this.appView.clearHistory();
+        this.appView.setWebViewClient(new CordovaWebViewClient(this, this.appView) {
+
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                if(url.contains("http://")) {
+                    //url = url.replace("app://", "");
+
+                    // DO STUFF NOTHING! Stay in our app please.
+
+                    return true;
+                } else {
+                    //view.loadUrl(url);
+                    return super.shouldOverrideUrlLoading(view, url);
+                }
+
+            }
+
+        });
         //setContentView(R.layout.main);
         super.loadUrl("file:///android_asset/www/index.html");
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
